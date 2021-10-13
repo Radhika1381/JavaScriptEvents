b7924521d2af695bd7f85b92e62b861b57c9d349
Radhika1381 <radhika.iyengar_cs20@gla.ac.in>
2021-10-13 09:35:53 +0530

    commit

---
+++ event.js
@@ -0,0 +1,17 @@
+function fun1(){
+    var x = document.getElementById("container");
+    var y = document.getElementById("p1");
+    x.style.backgroundColor = "Purple";
+    y.style.color="white";
+}
+function fun2(){
+    var x = document.getElementById("container");
+    var y = document.getElementById("p1");
+    x.style.backgroundColor = "yellow";
+    y.style.color ="black";
+}
+function fun3(){
+    var a = document.getElementById("text1");
+    a.style.border = "lightblue";
+    a.style.color = "lightblue";
+}
